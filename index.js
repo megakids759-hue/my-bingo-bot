433dac2dc33bfb47aadc4b38ee2ab40e81a74972
megakids759-hue <megakids759@gmail.com>
2026-02-17 14:33:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 const { Telegraf, Markup } = require('telegraf');
-const bot = new Telegraf('የአንተ_Bot_Token_እዚህ_ይግባ');
+
+// የእርስዎ Bot Token
+const bot = new Telegraf('8519763447:AAGSYAcMY4z8NKCXu7MJjFW-3jB1_LrwCbY');
 
 bot.start((ctx) => {
   const name = ctx.from.first_name;
-  
-  // ለተጫዋቹ የሚላክ ደህና መጣህ መልእክት
   const welcomeText = `እንኳን ወደ **ተወዳጅ ቢንጎ** በደህና መጡ! 🎰\n\nውድ ${name} በዚህ ቦት ተዝናንተው ያሸንፉ።`;
 
   // ልክ እንደ ቤተሰብ ቢንጎ አዝራሮችን መደርደር
   return ctx.replyWithMarkdown(welcomeText, 
     Markup.inlineKeyboard([
-      [Markup.button.callback('🎮 ጨዋታ ጀምር', 'play_now')],
-      [Markup.button.callback('💰 ገንዘብ አስገባ', 'deposit_now'), Markup.button.callback('💳 ሂሳብ እይ', 'check_balance')],
-      [Markup.button.callback('💸 ብር አውጣ', 'withdraw_now'), Markup.button.callback('📖 መመሪያ', 'show_rules')],
-      [Markup.button.callback('📞 እርዳታ', 'contact_admin')]
+      [Markup.button.webApp('🎮 ቢንጎ ተጫወት', 'https://megakids759-hue.github.io/bingo-app/')],
+      [Markup.button.callback('💰 ገንዘብ አስገባ', 'deposit'), Markup.button.callback('💳 ሂሳብ እይ', 'balance')],
+      [Markup.button.callback('💸 ብር አውጣ', 'withdraw'), Markup.button.callback('📖 መመሪያ', 'rules')],
+      [Markup.button.callback('📞 እርዳታ', 'support')]
     ])
   );
 });
 
 // አዝራሮቹ ሲነኩ የሚሰጡት መልስ
-bot.action('deposit_now', (ctx) => {
-  ctx.reply(`💰 **ገንዘብ ለማስገባት፡**\n\n📱 ቴሌብር፡ 0979596741\n👤 ስም፡ [wasihun]\n\nደረሰኝዎን እዚህ ይላኩ።`);
-});
-
-bot.action('show_rules', (ctx) => {
-  ctx.reply('📖 **መመሪያ፦**\n1. ብር ያስገቡ\n2. ካርታ ይቁረጡ\n3. በLive ቁጥሮችን ይከታተሉ!');
+bot.action('deposit', (ctx) => {
+  ctx.reply(`💰 **ገንዘብ ለማስገባት፡**\n\n📱 ቴሌብር፡ 0979596741\n👤 ስም፡ [Wasihun]\n\nደረሰኝዎን እዚህ ይላኩ።`);
 });
 
 bot.launch();
